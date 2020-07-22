c9e17a67f07d3ac8a9fec573670064c531aa4e46
Dennis Guse <dennis.guse@alumni.tu-berlin.de>
2020-07-22 22:47:52 +0200

    Enable StrictMode for debug builds.

---
+++ src/main/java/de/storchp/opentracks/osmplugin/Startup.java
@@ -0,0 +1,37 @@
+package de.storchp.opentracks.osmplugin;
+
+import android.app.Application;
+import android.os.StrictMode;
+import android.util.Log;
+
+/**
+ * Code that is executed when the application starts.
+ * <p>
+ * NOTE: How often actual application startup happens depends on the OS.
+ * Not every start of an activity will trigger this.
+ */
+public class Startup extends Application {
+
+    private static final String TAG = Startup.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Include version information into stack traces.
+        Log.i(TAG, BuildConfig.APPLICATION_ID + "; BuildType: " + BuildConfig.BUILD_TYPE + "; VersionName: " + BuildConfig.VERSION_NAME + "/" + " VersionCode: " + BuildConfig.VERSION_CODE);
+
+        //In debug builds: show thread and VM warnings.
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Enabling strict mode");
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build());
+        }
+    }
+}
