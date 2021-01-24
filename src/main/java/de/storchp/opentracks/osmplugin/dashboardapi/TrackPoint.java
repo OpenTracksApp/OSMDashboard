dfc462658b22e4c44e42599520d2562ef239f2ae
Peter Storch <peter.storch@gmail.com>
2021-01-24 21:42:12 +0100

    add trackpoint type to recognize pause/resume (#89)
    - read protocol version and choose right projection
    
    https://github.com/OpenTracksApp/OpenTracks/pull/553

--- src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
+++ src/main/java/de/storchp/opentracks/osmplugin/dashboardapi/TrackPoint.java
@@ -18,10 +18,11 @@ public class TrackPoint {
     public static final String LONGITUDE = "longitude";
     public static final String LATITUDE = "latitude";
     public static final String TIME = "time";
+    public static final String TYPE = "type";
 
     public static final double PAUSE_LATITUDE = 100.0;
 
-    public static final String[] PROJECTION = {
+    public static final String[] PROJECTION_V1 = {
             _ID,
             TRACKID,
             LATITUDE,
@@ -29,12 +30,21 @@ public class TrackPoint {
             TIME
     };
 
+    public static final String[] PROJECTION_V2 = {
+            _ID,
+            TRACKID,
+            LATITUDE,
+            LONGITUDE,
+            TIME,
+            TYPE
+    };
+
     private final long trackPointId;
     private final long trackId;
     private final LatLong latLong;
     private final boolean pause;
 
-    public TrackPoint(final long trackId, final long trackPointId, final double latitude, final double longitude) {
+    public TrackPoint(final long trackId, final long trackPointId, final double latitude, final double longitude, final Integer type) {
         this.trackId = trackId;
         this.trackPointId = trackPointId;
         if (MapUtils.isValid(latitude, longitude)) {
@@ -42,7 +52,7 @@ public class TrackPoint {
         } else {
             latLong = null;
         }
-        this.pause = latitude == PAUSE_LATITUDE;
+        this.pause = type != null ? type != 0 : latitude == PAUSE_LATITUDE;
     }
 
     public boolean hasValidLocation() {
@@ -57,9 +67,10 @@ public class TrackPoint {
      * Reads the TrackPoints from the Content Uri and split by segments.
      * Pause TrackPoints and different Track IDs split the segments.
      */
-    public static List<List<TrackPoint>> readTrackPointsBySegments(final ContentResolver resolver, final Uri data, final long lastTrackPointId) {
+    public static List<List<TrackPoint>> readTrackPointsBySegments(final ContentResolver resolver, final Uri data, final long lastTrackPointId, final int protocolVersion) {
         final List<List<TrackPoint>> segments = new ArrayList<>();
-        try (final Cursor cursor = resolver.query(data, TrackPoint.PROJECTION, TrackPoint._ID + "> ?", new String[]{Long.toString(lastTrackPointId)}, null)) {
+        final String[] projection = protocolVersion < 2 ? PROJECTION_V1 : PROJECTION_V2;
+        try (final Cursor cursor = resolver.query(data, projection, TrackPoint._ID + "> ?", new String[]{Long.toString(lastTrackPointId)}, null)) {
             TrackPoint lastTrackPoint = null;
             List<TrackPoint> segment = null;
             while (cursor.moveToNext()) {
@@ -67,13 +78,18 @@ public class TrackPoint {
                 final long trackId = cursor.getLong(cursor.getColumnIndex(TrackPoint.TRACKID));
                 final double latitude = cursor.getInt(cursor.getColumnIndex(TrackPoint.LATITUDE)) / 1E6;
                 final double longitude = cursor.getInt(cursor.getColumnIndex(TrackPoint.LONGITUDE)) / 1E6;
+                final int typeIndex = cursor.getColumnIndex(TrackPoint.TYPE);
+                Integer type = null;
+                if (typeIndex > -1) {
+                    type = cursor.getInt(typeIndex);
+                }
 
                 if (lastTrackPoint == null || lastTrackPoint.trackId != trackId) {
                     segment = new ArrayList<>();
                     segments.add(segment);
                 }
 
-                lastTrackPoint = new TrackPoint(trackId, trackPointId, latitude, longitude);
+                lastTrackPoint = new TrackPoint(trackId, trackPointId, latitude, longitude, type);
                 if (lastTrackPoint.hasValidLocation()) {
                     segment.add(lastTrackPoint);
                 }
