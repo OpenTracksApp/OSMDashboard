be05f666a46933cfa77929ca306d72642ce4d67d
Peter Storch <peter.storch@tuta.io>
2024-10-19 22:14:20 +0200

    support Full|Offline flavors for release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+# usage:
+#   ./release.sh [Full|Offline] keystore_password
 
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk/
 export PATH=$JAVA_HOME/bin:$PATH
 
-./gradlew --no-configuration-cache clean assembleFullRelease -Drelease_store_file=/home/peter/upload-android.jks -Drelease_store_password="$1" -Drelease_key_alias='upload' -Drelease_key_password="$1"
+./gradlew --no-configuration-cache clean "assemble${1}Release" -Drelease_store_file=/home/peter/upload-android.jks -Drelease_store_password="$2" -Drelease_key_alias='upload' -Drelease_key_password="$2"
